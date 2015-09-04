e8ec3e28e7e3c346425e6e4dd6373ecc9c35797b
potikanond <potikanond@gmail.com>
2015-09-04 16:22:38 +0700

    Add Lab301 requirements: - README.md - Program.cs

---
+++ Lab/Lab301/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Lab301
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Fraction r1 = new Fraction();
+			Fraction r2 = new Fraction(2);
+			Fraction r3 = new Fraction(3,9);
+
+			Console.WriteLine ("***** " + Fraction.Count + " Fraction objects have been created *****");
+
+			Fraction r4 = r1+r2;
+			Fraction r5 = r2-r3;
+			Fraction r6 = new Fraction(100,50);
+			r6 += r2;
+
+			Fraction r7 = new Fraction(r3);		// case#1
+//			Fraction r7 = r3;					// case#2
+			r7++;
+
+			Console.WriteLine ("***** " + Fraction.Count + " Fraction objects have been created *****");
+
+			Fraction r8 = 10 - (new Fraction(80,40));
+			Fraction r9 = (new Fraction(5,25)) + 1;
+
+			Console.WriteLine ("***** " + Fraction.Count + " Fraction objects have been created *****");
+
+			Console.WriteLine (r1);
+			Console.WriteLine (r2);
+			Console.WriteLine (r3);
+			Console.WriteLine (r4);
+			Console.WriteLine (r5);
+			Console.WriteLine (r6);
+			Console.WriteLine (r7);
+			Console.WriteLine (r8);
+			Console.WriteLine (r9);
+
+			Console.WriteLine (r2==r4);
+			Console.WriteLine (r4.Equals (r2)); // Equals method must be overriden
+			Console.WriteLine (r2!=r4);
+
+			r3.setValue (10,12/3*0);
+			Console.WriteLine (r3);
+
+			Console.WriteLine ("***** " + Fraction.Count + " Fraction objects have been created *****");
+
+			Console.WriteLine("GCD of 3650 and 360: " + Fraction.GCD (3650,360));
+			Console.WriteLine("GCD of 3600 and 360: " + Fraction.GCD (3600,360));
+
+		}
+	}
+}
---
+++ Lab/Lab301/README.md
@@ -0,0 +1,50 @@
+﻿# Implement the Fraction class according to the following requirements:
+
+## Propeties:
+- Numer: Numerator
+- Denom: Denominator (default=1, cannot be 0)
+- Count: counting #objects of this class (static)
+Note: Numerator and Denominator have to in the minimal form.
+      See r3 and r6.
+
+## Constructors:
+- Fraction (): default constructor
+- Fraction (Fraction a): copy constructor
+- Fraction (numerator, denominator)
+note: increment the Count property when an object is created
+
+## Methods
+- setValue: set fraction value
+- GCD: calculate Greatest Common Divisor of two integers (static)
+
+## Operator Overloading:
+many many operators need to be overloaded!!! 
+
+## Question?
+1. Is the number of Fraction objects equal to the 'new' keywords used
+   in the 'main' methods? How come?
+2. Are the result, r3 and r7, in case#1 and case#2 the same? Why?
+
+
+## Expected Output:
+
+***** 3 Fraction objects have been created *****
+***** 8 Fraction objects have been created *****
+***** 12 Fraction objects have been created *****
+[Rational: 0/1], value=0]
+[Rational: 2/1], value=2]
+[Rational: 1/3], value=0.333333333333333]
+[Rational: 2/1], value=2]
+[Rational: 5/3], value=1.66666666666667]
+[Rational: 4/1], value=4]
+[Rational: 4/3], value=1.33333333333333]
+[Rational: 8/1], value=8]
+[Rational: 6/5], value=1.2]
+True
+True
+False
+[Rational: 10/1], value=10]
+***** 12 Fraction objects have been created *****
+GCD of 3650 and 360: 10
+GCD of 3600 and 360: 360
+
