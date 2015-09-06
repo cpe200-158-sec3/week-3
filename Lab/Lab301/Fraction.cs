c37f32d3db4c139f58ec9f497b3c2b1fe9810c8e
Nrtdemo <nrtdemo@hotmail.com>
2015-09-06 14:48:43 +0700

    add file.

---
+++ Lab/Lab301/Fraction.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab301
+{
+    class Fraction
+    {
+        private double _numer, _denom;
+        public double Numer
+        {
+            get { return _numer; }
+            set { _numer = value; }
+        }
+        public double Denom
+        {
+            get { return _denom; }
+            set { _denom = value; }
+        }
+        private static int _count = 0;
+        public static int Count
+        {
+            get { return _count; }
+        }
+        public Fraction()
+        {
+            this.Numer = 0;
+            this.Denom = 1;
+            _count++;
+        }
+        public Fraction(double numer)
+        {
+            this.Numer = numer;
+            this.Denom = 1;
+            _count++;
+        }
+        public Fraction(double numer, double denom)
+        {
+            this.Numer = numer;
+            this.Denom = denom;
+            _count++;
+        }
+        public Fraction(Fraction f)
+        {
+            this.Numer = f.Numer;
+            this.Denom = f.Denom;
+        }
+        public static Fraction operator +(Fraction f1,Fraction f2)
+        {
+            return new Fraction((f1.Numer*f2.Denom+f2.Numer*f1.Denom)/(f1.Denom*f2.Denom));
+        }
+        public static Fraction operator -(Fraction f1, Fraction f2)
+        {
+            return new Fraction((f1.Numer * f2.Denom - f2.Numer * f1.Denom) / (f1.Denom * f2.Denom));
+        }
+        public static Fraction operator ++(Fraction f)
+        {
+            return new Fraction((f.Numer + f.Denom)/f.Denom);
+        }
+
+        public static Fraction operator +(Fraction f,int x)
+        {
+            return new Fraction((f.Numer + (f.Denom * x)) / (f.Denom));
+        }
+        public static Fraction operator +(int x, Fraction f)
+        {
+            return new Fraction((f.Numer + (f.Denom * x)) / (f.Denom));
+        }
+        public static Fraction operator -(int x, Fraction f)
+        {
+            return new Fraction(((f.Denom * x) - f.Numer) / (f.Denom));
+        }
+        public static Fraction operator -(Fraction f, int x)
+        {
+            return new Fraction((f.Numer - (f.Denom * x)) / (f.Denom));
+        }
+
+        //public static Fraction operator =(Fraction f)
+        //{
+        //    return new Fraction();
+        //}
+        public override string ToString()
+        {
+            string s = "[Rational: " + this.Numer + "/" + this.Denom + "], value=" + (this.Numer / this.Denom) + "]";
+            return s;
+        }
+    }
+}
--- Lab/Lab301/Program.cs
+++ Lab/Lab301/Program.cs
@@ -23,10 +23,10 @@ namespace Lab301
 
 			Console.WriteLine ("***** " + Fraction.Count + " Fraction objects have been created *****");
 
-			Fraction r8 = 10 - (new Fraction(80,40));
-			Fraction r9 = (new Fraction(5,25)) + 1;
+            Fraction r8 = 10 - (new Fraction(80, 40));
+            Fraction r9 = (new Fraction(5, 25)) + 1;
 
-			Console.WriteLine ("***** " + Fraction.Count + " Fraction objects have been created *****");
+            Console.WriteLine ("***** " + Fraction.Count + " Fraction objects have been created *****");
 
 			Console.WriteLine (r1);
 			Console.WriteLine (r2);
@@ -35,21 +35,21 @@ namespace Lab301
 			Console.WriteLine (r5);
 			Console.WriteLine (r6);
 			Console.WriteLine (r7);
-			Console.WriteLine (r8);
-			Console.WriteLine (r9);
+            Console.WriteLine (r8);
+            Console.WriteLine (r9);
 
-			Console.WriteLine (r2==r4);
-			Console.WriteLine (r4.Equals (r2)); // Equals method must be overriden
-			Console.WriteLine (r2!=r4);
+            //Console.WriteLine(r2 == r4);
+            //Console.WriteLine(r4.Equals(r2)); // Equals method must be overriden
+            //Console.WriteLine(r2 != r4);
 
-			r3.setValue (10,12/3*0);
-			Console.WriteLine (r3);
+            //r3.setValue(10, 12 / 3 * 0);
+            //Console.WriteLine(r3);
 
-			Console.WriteLine ("***** " + Fraction.Count + " Fraction objects have been created *****");
+            //Console.WriteLine("***** " + Fraction.Count + " Fraction objects have been created *****");
 
-			Console.WriteLine("GCD of 3650 and 360: " + Fraction.GCD (3650,360));
-			Console.WriteLine("GCD of 3600 and 360: " + Fraction.GCD (3600,360));
-
+            //Console.WriteLine("GCD of 3650 and 360: " + Fraction.GCD(3650, 360));
+            //Console.WriteLine("GCD of 3600 and 360: " + Fraction.GCD(3600, 360));
+            Console.ReadKey();
 		}
 	}
 }
