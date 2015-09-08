f48f1bc90daaedab11db5c51f46dc6cd3442262f
toptenshadow <topten_non-prince@hotmail.com>
2015-09-08 16:32:39 +0700

    Fraction to fraction

--- Lab/Lab301/Program.cs
+++ Lab/Lab301/Program.cs
@@ -2,54 +2,56 @@
 
 namespace Lab301
 {
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
-			Fraction r1 = new Fraction();
-			Fraction r2 = new Fraction(2);
-			Fraction r3 = new Fraction(3,9);
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            fraction r1 = new fraction();
+            fraction r2 = new fraction(2);
+            fraction r3 = new fraction(3, 9);
 
-			Console.WriteLine ("***** " + Fraction.Count + " Fraction objects have been created *****");
+            Console.WriteLine("***** " + fraction.Count + " Fraction objects have been created *****");
 
-			Fraction r4 = r1+r2;
-			Fraction r5 = r2-r3;
-			Fraction r6 = new Fraction(100,50);
-			r6 += r2;
+            fraction r4 = r1 + r2;
+            fraction r5 = r2 - r3;
+            fraction r6 = new fraction(100, 50);
+            r6 += r2;
 
-			Fraction r7 = new Fraction(r3);		// case#1
-//			Fraction r7 = r3;					// case#2
-			r7++;
+            fraction r7 = new fraction(r3);		// case#1     
+            //fraction r7 = r3;					// case#2
+            r7++;
 
-			Console.WriteLine ("***** " + Fraction.Count + " Fraction objects have been created *****");
+            Console.WriteLine("***** " + fraction.Count + " Fraction objects have been created *****");
 
-			Fraction r8 = 10 - (new Fraction(80,40));
-			Fraction r9 = (new Fraction(5,25)) + 1;
+            fraction r8 = 10 - (new fraction(80, 40));
+            fraction r9 = (new fraction(5, 25)) + 1;
 
-			Console.WriteLine ("***** " + Fraction.Count + " Fraction objects have been created *****");
+            Console.WriteLine("***** " + fraction.Count + " Fraction objects have been created *****");
 
-			Console.WriteLine (r1);
-			Console.WriteLine (r2);
-			Console.WriteLine (r3);
-			Console.WriteLine (r4);
-			Console.WriteLine (r5);
-			Console.WriteLine (r6);
-			Console.WriteLine (r7);
-			Console.WriteLine (r8);
-			Console.WriteLine (r9);
+            Console.WriteLine(r1);
+            Console.WriteLine(r2);
+            Console.WriteLine(r3);
+            Console.WriteLine(r4);
+            Console.WriteLine(r5);
+            Console.WriteLine(r6);
+            Console.WriteLine(r7);
+            Console.WriteLine(r8);
+            Console.WriteLine(r9);
 
-			Console.WriteLine (r2==r4);
-			Console.WriteLine (r4.Equals (r2)); // Equals method must be overriden
-			Console.WriteLine (r2!=r4);
+            Console.WriteLine(r2 == r4);
+            Console.WriteLine(r4.Equals(r2)); // Equals method must be overriden
+            Console.WriteLine(r2 != r4);
 
-			r3.setValue (10,12/3*0);
-			Console.WriteLine (r3);
+            r3.setValue(10, 12 / 3 * 0);
+            Console.WriteLine(r3);
 
-			Console.WriteLine ("***** " + Fraction.Count + " Fraction objects have been created *****");
+            Console.WriteLine("***** " + fraction.Count + " Fraction objects have been created *****");
 
-			Console.WriteLine("GCD of 3650 and 360: " + Fraction.GCD (3650,360));
-			Console.WriteLine("GCD of 3600 and 360: " + Fraction.GCD (3600,360));
+            Console.WriteLine("GCD of 3650 and 360: " + fraction.GCD(3650, 360));
+            Console.WriteLine("GCD of 3600 and 360: " + fraction.GCD(3600, 360));
 
-		}
-	}
+            Console.ReadLine();
+        }
+
+    }
 }
